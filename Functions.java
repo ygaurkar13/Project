0ead3fc91528a123bac098fb63d431557a85a7ae
ygaurkar1311 <2022bit021@sggs.ac.in>
2024-01-23 23:21:45 +0530

    Basic functions

---
+++ Functions.java
@@ -0,0 +1,36 @@
+import java.lang.Math;
+
+public class Calci{
+
+public static void main(String[] args){
+
+double n1=30,n2=50;
+int mul=multiply(n1,n2);
+}
+static int multiply(int a,int b){
+       return a*b;
+}
+static int Subtract(int a,int b){
+       return a-b;
+}
+
+static int Addition(int a,int b){
+       return a+b;
+}
+
+static int Division(int a,int b){
+       return a/b;
+}
+static int modulus(int a,int b){
+       return a%b;
+}
+static int Square(int a){
+       return a*a;
+}
+static int Cube(int a){
+	return a*a*a;
+}
+
+
+
+
