e80a02c18e4d6d8af6b2ae05aa924ab72eee4234
ygaurkar1311 <2022bit021@sggs.ac.in>
2024-01-24 00:04:09 +0530

    First function

--- Functions.java
+++ Functions.java
@@ -7,10 +7,10 @@ public static void main(String[] args){
 double n1=30,n2=50;
 int mul=multiply(n1,n2);
 }
-static int multiply(int a,int b){
+static int multiply(int a,int b){ //function to multiply to numbers
        return a*b;
 }
-
+     
 
 
 
