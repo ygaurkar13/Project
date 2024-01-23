3a6dc4345eb573b0aaa27b3cb4dc2d42f0340f8a
ygaurkar1311 <2022bit021@sggs.ac.in>
2024-01-23 23:44:33 +0530

    First function

--- Functions.java
+++ Functions.java
@@ -10,26 +10,7 @@ int mul=multiply(n1,n2);
 static int multiply(int a,int b){
        return a*b;
 }
-static int Subtract(int a,int b){
-       return a-b;
-}
 
-static int Addition(int a,int b){
-       return a+b;
-}
-
-static int Division(int a,int b){
-       return a/b;
-}
-static int modulus(int a,int b){
-       return a%b;
-}
-static int Square(int a){
-       return a*a;
-}
-static int Cube(int a){
-	return a*a*a;
-}
 
 
 
