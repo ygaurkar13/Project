ed41a27d1e0333e3f244fe47e780eb1e4a674635
ygaurkar1311 <2022bit021@sggs.ac.in>
2024-01-24 09:49:11 +0530

    Second Function

--- Functions.java
+++ Functions.java
@@ -4,13 +4,16 @@ public class Calci{
 
 public static void main(String[] args){
 
-double n1=30,n2=50;
-int mul=multiply(n1,n2);
+double a=30,b=50;
+int mul=multiply(a,b);
+int diff=Subtract(a,b);
 }
 static int multiply(int a,int b){ //function to multiply to numbers
        return a*b;
 }
-     
+     static int Subtract(int a,int b){
+       return a-b;
+}
 
 
 
